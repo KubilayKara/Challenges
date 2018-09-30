72c01843835a16b1e9236ed7b240c7e38a0fd475
KubilayKara <kara.kubilay@gmail.com>
2018-09-30 22:15:24 +0300

    test projects

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the checkMagazine function below.
+    static void checkMagazine(string[] magazine, string[] note)
+    {
+        string Result = "Yes";
+        var foos = new List<string>(magazine.Length);
+        foos = magazine.ToList();
+
+        var notes = new List<string>(note.Length);
+        notes = note.ToList();
+
+        var howmanynote = new List<string>(note.Length) ;
+        var howmanymag = new List<string>(magazine.Length);
+
+
+
+
+        for (int i = 0; i < note.Length; i++)
+        {
+
+   
+            int indexx = foos.IndexOf(note[i]);
+
+            howmanynote = notes.FindAll(delegate (string s) { return s == note[i]; });
+            howmanymag = foos.FindAll(delegate (string t) { return t == note[i]; });
+
+            if(howmanymag.Count< howmanynote.Count)
+            {
+
+                Result = "No";
+                break;
+            }
+              
+
+            //int indexx = Array.IndexOf(magazine, note[i]);
+            if (indexx>=0)
+            {
+
+                //Array.Clear(magazine, indexx, 1);
+                
+                    
+                    foos.RemoveAt(indexx);
+                
+            }
+            else
+            {
+                Result = "No";
+                break;
+            }
+
+      
+        }
+
+        Console.WriteLine(Result);
+     
+
+    }
+
+    static void Main(string[] args)
+    {
+        string[] mn = Console.ReadLine().Split(' ');
+
+        int m = Convert.ToInt32(mn[0]);
+
+        int n = Convert.ToInt32(mn[1]);
+
+        string[] magazine = Console.ReadLine().Split(' ');
+
+        string[] note = Console.ReadLine().Split(' ');
+
+        checkMagazine(magazine, note);
+    }
+}
---
+++ HackerRank/HackerRankTestConsoleApp/MaxArraySum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankTestConsoleApp
+{
+    public class MaxArraySum
+    {
+        //https://www.hackerrank.com/challenges/max-array-sum/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dynamic-programming
+
+        public void Test()
+        {
+            Console.WriteLine("Sample Input 1:" + maxSubsetSum(new int[] { 2, 1, 5, 8, 4 }));
+            Console.WriteLine("Sample Input 2:" + maxSubsetSum(new int[] { 3, 5, -7, 8, 10 }));
+            Console.WriteLine("Sample Input 3:" + maxSubsetSum(new int[] { 2, 3, 2, -7, 8, 10 }));
+            Console.WriteLine("Sample Input 4:" + maxSubsetSum(new int[] { 2, -3, 2, -7, 8, 10 }));
+            Console.WriteLine("Sample Input 5:" + maxSubsetSum(new int[] { 2,1,8, 10, 8 }));
+        }
+
+        // Complete the maxSubsetSum function below.
+        static int maxSubsetSum(int[] arr)
+        {
+            //discard negatives
+            //
+
+        }
+    }
+}
